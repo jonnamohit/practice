e00a22007832e16365847fdc6cdc56f6862b479f
Jonna Mohit <luckymohit789@gmail.com>
2024-12-05 19:33:11 +0530

    Add files via upload

---
+++ rotate2d.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class rotate2d {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int [][]arr=new int[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=n-1;j>=0;j--){
+                System.out.print(" "+arr[j][i]);
+            }
+            System.out.println("");
+        }
+
+
+    }
+    
+}
